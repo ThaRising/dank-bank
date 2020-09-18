4e38adc22e1a859ce9b3bf931408f97e930746cc
ThaRising <kochbe.ber@gmail.com>
2020-09-18 15:06:42 +0200

    Added additional test table

--- src/models.py
+++ src/models.py
@@ -1,11 +1,16 @@
 import sqlalchemy as sqla
-from .storage.datastores.sql import Base
+from .storage.sql import Base
 
 
 class Benutzer(Base):
     id = sqla.Column(sqla.Integer, primary_key=True)
     vorname = sqla.Column(sqla.String)
     nachname = sqla.Column(sqla.String)
+
+
+class TestTable(Base):
+    name = sqla.Column(sqla.String, primary_key=True)
+    sth = sqla.Column(sqla.Integer)
 
 
 __all__ = ["Benutzer"]

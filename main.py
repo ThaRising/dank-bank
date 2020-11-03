00eb9df24b6debf117247d1f2502a9edb1756872
ThaRising <kochbe.ber@gmail.com>
2020-11-03 20:33:21 +0100

    Test data

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from src import Storage
+from src import get_storage
 
 if __name__ == '__main__':
+    """
     db = Storage(
         "sql", conn_args_overrides={
             "dialect": "sqlite",
             "host": "data.sqlite3"
         }
     )
+    """
+    db = get_storage("json")
     db.create()

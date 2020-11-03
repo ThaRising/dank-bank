89fa4c88d961f1cae1d5f3d8f29f33f9904f9a29
ThaRising <kochbe.ber@gmail.com>
2020-11-03 20:32:38 +0100

    Fixed circular import

--- src/__init__.py
+++ src/__init__.py
@@ -1,33 +1,15 @@
-from typing import Union, Optional
-
 from drizm_commons.sqla import Database
 
 from . import models
-from .interfaces import get_controller
+from .controllers import get_controller
 from .json import JsonAdapter
 
 
-class Storage:
-    __state = None
-
-    def __new__(cls, /,
-                storage_type: str = None,
-                *, conn_args_overrides: Optional[dict] = None
-                ) -> Union[Database, JsonAdapter]:
-        if cls.__state:
-            return cls.__state
-        if not storage_type:
-            raise ValueError("storage_type parameter is required")
-
-        if storage_type == "sql":
-            if conn_args_overrides:
-                cls.__state = Database(conn_args=conn_args_overrides)
-            cls.__state = Database(dialect="sqlite")
-        elif storage_type == "json":
-            cls.__state = JsonAdapter()
-        else:
-            raise TypeError(f"storage_type {storage_type} not supported")
-        return cls.__state
+def get_storage(storage_type: str):
+    if storage_type == "sql":
+        return Database(dialect="sqlite")
+    elif storage_type == "json":
+        return JsonAdapter()
 
 
-__all__ = ["Storage", "get_controller", "models"]
+__all__ = ["get_storage", "get_controller", "models"]

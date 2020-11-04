2f250f92201f734875d9b22d60796e22d498feab
ThaRising <kochbe.ber@gmail.com>
2020-11-04 19:29:36 +0100

    Added simple test scenario

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from src import get_storage
+import datetime
+
+from src import get_storage, get_controller
+from src.models import Kunde
 
 if __name__ == '__main__':
-    """
-    db = Storage(
-        "sql", conn_args_overrides={
-            "dialect": "sqlite",
-            "host": "data.sqlite3"
-        }
-    )
-    """
     db = get_storage("json")
     db.create()
+    k = Kunde(
+        name="Ben Koch",
+        adresse="iubuhbijgkijnkjn",
+        geb_date=datetime.date.today()
+    )
+    c = get_controller(db)
+    C = c(k, db)
+    C.create()
+    c.read(Kunde, db, pk=k.pk)

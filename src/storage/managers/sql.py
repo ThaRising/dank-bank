488eb19c5192967590072fd8998973eda19e431b
ThaRising <kochbe.ber@gmail.com>
2020-12-01 18:16:15 +0100

    Refactored SQL Manager class

--- src/storage/managers/sql.py
+++ src/storage/managers/sql.py
@@ -1,40 +1,36 @@
+from typing import Union
+
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
 from .base import BaseManagerInterface
 
 
 class SqlManager(BaseManagerInterface):
-    def _get_storage(self):
-        from ..storage import Storage
-        return Storage()
-
     def save(self) -> None:
         with self.db.Session() as sess:
-            sess.add(self.model_instance)
+            sess.add(self.klass)
 
     def update(self, data: dict) -> None:
         with self.db.Session():
             for k, v in data.items():
-                setattr(self.model_instance, k, v)
+                setattr(self.klass, k, v)
 
     def delete(self, **kwargs) -> None:
         with self.db.Session() as sess:
-            sess.delete(self.model_instance)
+            sess.delete(self.klass)
 
-    def read(model_class: DeclarativeMeta,
-             *args, **kwargs) -> Union[list, DeclarativeMeta]:
-        from . import get_storage
-        storage = get_storage()
-
+    def read(self, *args, **kwargs) -> Union[list, DeclarativeMeta]:
         # if the user has not provided any kwargs like 'pk=3'
         # then we can assume they want all rows of the given table
         if not kwargs and not args:
-            with storage.Session() as sess:
-                return sess.query(model_class).all()
+            with self.db.Session() as sess:
+                return sess.query(self.klass.__class__).all()
 
         # if they did provide kwargs we can filter
         if kwargs:
-            with storage.Session() as sess:
-                return sess.query(model_class).filter_by(**kwargs).all()
+            with self.db.Session() as sess:
+                return sess.query(self.klass).filter_by(**kwargs).all()
 
         elif args:
-            with storage.Session() as sess:
-                return sess.query(model_class).get(args[0])
+            with self.db.Session() as sess:
+                return sess.query(self.klass).get(args[0])

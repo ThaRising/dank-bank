634af7e61ecfa8c1cdca4b3f56538e5b4ab47bbb
ThaRising <kochbe.ber@gmail.com>
2020-12-02 00:06:48 +0100

    Adjusted test code to new API

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 import datetime
 
-from src.storage import get_storage, get_controller
-from src.storage.models import Kunde, Konto
+from src.storage import Storage
+from src.storage.models import Kunde
+
 
 if __name__ == '__main__':
-    db = get_storage("json")
-    db.create()
-    Controller = get_controller(db)
-    kunde = Kunde(
-        name="Ben Koch",
-        plz="16386",
-        stadt="Berlin",
-        strasse="fgr rgtert ergert ergtert",
-        geb_date=datetime.date(2000, 1, 1)
-    )
-    kunde_controller = Controller(kunde)
-    kunde_controller.create()
-    konto = Konto(
-        besitzer=kunde.pk
-    )
-    konto_controller = Controller(konto)
-    konto_controller.create()
-    print(kunde.konten)
-
-    Storage("json")
+    Storage("sql")
     kunde = Kunde(
         name="Ben Koch",
         plz="16386",
@@ -32,3 +14,5 @@ if __name__ == '__main__':
         geb_date=datetime.date(2000, 1, 1)
     )
     kunde.objects.save()
+    print(kunde.objects.all())
+    print(Kunde.objects.all())

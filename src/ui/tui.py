7ca41513e057f97102eb4eb2e072ba400621e870
j0fz <rene.produktiv@gmx.de>
2020-12-02 18:14:16 +0100

    -corrected existing "try except" catches -added new ones for fool proofing

--- src/ui/tui.py
+++ src/ui/tui.py
@@ -18,17 +18,7 @@ class TUI:
                     pass
                 self.option = option
 
-                if option == "Einzahlung":
-                    self.option_deposit()
-                elif option == "Auszahlung":
-                    self.option_withdraw()
-                elif option == "Überweisung":
-                    self.option_transfer()
-                elif option == "Kontostand":
-                    self.option_balance()
-                else:
-                    print("öhhm etwas ist schief gelaufen")
-                    sys.exit(69)
+                self.pass_option(option)
 
                 if not self.keep_running():
                     sys.exit(0)
@@ -41,8 +31,8 @@ class TUI:
         print('1 = SQL | 2 = JSON')
         try:
             file_type = int(input())
-        except TypeError:
-            print("Bitte geben sie einen gültigen Integer ein, danke Ronneberg")
+        except ValueError:
+            print("Bitte geben sie einen gültigen Integer ein!\n")
             return False
         if file_type == 1:
             print('SQL wurde gewählt')
@@ -51,16 +41,26 @@ class TUI:
             print('JSON wurde gewählt')
             return file_type
         else:
-            print('Absolut kritisch mein Freund')
+            print('Bitte geben sie eine gültigen Wert ein!\n')
 
     def login_prompt(self) -> Optional[int]:
         # idgaf about this method, get rekt
         username = str(input('Username: '))
-        account_id = int(input('Account ID: '))
+        account_id = self.login_account_id_check()
         password = str(input('Passwort: '))
-        print(username, account_id, password)
+        print(username, account_id, password, '\n')
         conUser = True
         return conUser
+
+    def login_account_id_check(self):
+        while True:
+            try:
+                account_id = int(input('Account ID: '))
+            except ValueError:
+                print('Ungültige Eingabe! Bitte Integer Wert eingeben.\n')
+            else:
+                break
+        return account_id
 
     def choose_option(self) -> Optional[int]:
         options = [
@@ -83,20 +83,33 @@ class TUI:
             print("Ist keine gültige Option")
             return
 
+    def pass_option(self, option):
+        if option == "Einzahlung":
+            self.option_deposit()
+        elif option == "Auszahlung":
+            self.option_withdraw()
+        elif option == "Überweisung":
+            self.option_transfer()
+        elif option == "Kontostand":
+            self.option_balance()
+        else:
+            print("öhhm etwas ist schief gelaufen")
+            sys.exit(69)
+
     def option_deposit(self):
-        print('Einzahlung wurde gewählt')
+        print('Einzahlung wurde gewählt\n')
         #TODO Einzahlung
 
     def option_withdraw(self):
-        print('Auszahlung wurde gewählt')
+        print('Auszahlung wurde gewählt\n')
         #TODO Auszahlung
 
     def option_transfer(self):
-        print('Überweisung wurde gewählt')
+        print('Überweisung wurde gewählt\n')
         #TODO Überweisung
 
     def option_balance(self):
-        print('Kontostand wurde gewählt')
+        print('Kontostand wurde gewählt\n')
         #TODO Kontostand
 
     def change_user_or_stay_logged_in(self) -> Optional[int]:
@@ -105,9 +118,9 @@ class TUI:
                 input('Weiter Aktionen mit diesen Account tätigen?\n1 = Ja | 2 = Nein\n')
             )
             return option - 1
-        except TypeError:
-            print("Bitte geben sie einen gültigen Integer ein, danke Ronneberg")
-            return False
+        except ValueError:
+            print("Ungültiger Input! Sie werden nun zurück zur Anmeldung gebracht")
+            return 1
 
     def keep_running(self) -> Optional[int]:
         try:
@@ -115,9 +128,9 @@ class TUI:
                 input('Programm beenden?\n1 = Ja | 2 = Nein\n')
             )
             return option - 1
-        except TypeError:
-            print("Programm wird fortgefahren")
-            return False
+        except ValueError:
+            print("Ungültiger Input! Programm wird fortgefahren")
+            return 1
 
 
 if __name__ == '__main__':

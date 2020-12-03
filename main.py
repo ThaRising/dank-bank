f7996bd1166f9552ca844a01d5b9e02a09af682b
ThaRising <kochbe.ber@gmail.com>
2020-12-03 01:17:27 +0100

    Adjusted sample code

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import datetime
 
 from src.storage import Storage
-from src.storage.models import Kunde
+from src.storage.models import Kunde, Konto
 
 
 if __name__ == '__main__':
     Storage("sql")
+    Storage().db.create()
+
     kunde = Kunde(
         name="Ben Koch",
+        username="ben.koch",
+        password="security420",
         plz="16386",
         stadt="Berlin",
         strasse="fgr rgtert ergert ergtert",

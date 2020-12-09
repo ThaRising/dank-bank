9a26581f80e3082ee2208dcc1d79350e1f5de7a7
ThaRising <kochbe.ber@gmail.com>
2020-12-09 02:20:42 +0100

    Major refactor for JSONManager with some temporary fixes for deserialization

--- src/storage/managers/json.py
+++ src/storage/managers/json.py
@@ -1,35 +1,30 @@
 import json
 from pathlib import Path
-from typing import Union, List, Optional
-from inspect import isclass
+from copy import deepcopy
+import datetime
 
 from drizm_commons.inspect import SQLAIntrospector
 from drizm_commons.sqla import Registry, Base, SqlaDeclarativeEncoder
-from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from .base import BaseManagerInterface
-
-
-def find_index_by_value_at_key(items: List[dict],
-                               key,
-                               value) -> Optional[int]:
-    """
-    Inside of a list of dictionaries,
-    find the index of the first dictionary,
-    that has a matching value for the given key
-    """
-    for index, item in enumerate(items):
-        if item.get(key) == value:
-            return index
-    return None
+from ..exc import ObjectAlreadyExists, ObjectNotFound
+from ..utils import find_index_by_value_at_key
 
 
 class JsonManager(BaseManagerInterface):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.inspect = SQLAIntrospector(self.klass)
+
+        self.filename = self.db.schema[self.inspect.tablename]["file"]
+        self.filepath = self.db.path / self.filename
+
     # noinspection PyMethodMayBeStatic
-    def _read_file_contents(self, filepath: Path):
+    def _read_file_contents(self):
         # the default JSON decoder does not accept a file,
         # that has just an empty list in it so we ignore the error
-        with open(filepath, "r") as fin:
+        with open(self.filepath, "r") as fin:
             try:
                 content = json.load(fin)
             except ValueError:
@@ -39,6 +34,18 @@ class JsonManager(BaseManagerInterface):
     # noinspection PyMethodMayBeStatic
     def _save(self, current_content, fout) -> None:
         """ Writes the provided content into the provided file-object """
+        # TODO all of this is just a band-aid fix
+        for index, elem in enumerate(current_content):
+            if type(elem) != dict:
+                obj = {
+                    attr: getattr(
+                        elem, attr
+                    ) for attr in elem.__dict__ if not attr.startswith("_")
+                }
+                for k, v in obj.items():
+                    if type(v) == datetime.date or type(v) == datetime.datetime:
+                        obj[k] = v.isoformat()
+                current_content[index] = obj
         json.dump(
             current_content,
             fout,
@@ -46,14 +53,39 @@ class JsonManager(BaseManagerInterface):
             cls=SqlaDeclarativeEncoder
         )
 
-    def save(self):
-        table = SQLAIntrospector(self.klass)
-        filename = self.db.schema[table.tablename]["file"]
-        filepath = self.db.path / filename
+    def _check_unique(self, current_content: dict) -> None:
+        unique_keys = self.inspect.unique_keys()
 
-        with open(filepath, "w") as fout:
+        # Get the values of the unique columns on our instance
+        unique_values = {
+            key: getattr(self.klass, key) for key in unique_keys
+        }
+
+        for entity in current_content:
+            entity_unique_values = [
+                getattr(entity, key) for key in unique_keys
+            ]
+            for (attr_name, instance_value), entity_value in zip(
+                unique_values.items(), entity_unique_values
+            ):
+                if instance_value == entity_value:
+                    raise ObjectAlreadyExists(
+                        f"Value '{instance_value}' for Column "
+                        f"'{attr_name}' of model "
+                        f"'{self.klass.__class__.__name__}' is not unique."
+                    )
+
+    def save(self):
+        with open(self.filepath, "w") as fout:
             # read the file -> add some content -> overwrite the file
-            current_content = self._read_file_contents(filepath)
+            current_content = self._read_file_contents()
+
+            # Make sure that there is no instance with
+            # the same unique fields already here
+            pk_column = self._get_identifier_column_name()
+            instance_pk = self._get_identifier()
+            unique_keys = self.inspect.unique_keys()
+
             current_content.append(self.klass)
             self._save(current_content, fout)
 
@@ -62,78 +94,95 @@ class JsonManager(BaseManagerInterface):
         for k, v in data.items():
             setattr(self.klass, k, v)
 
-        table = SQLAIntrospector(self.klass)
-        filename = self.db.schema[table.tablename]["file"]
-
-        with open((self.db.path / filename), "w") as fout:
+        with open(self.filepath, "w") as fout:
             # read the file -> modify the content -> overwrite the file
             current_content = json.load(fout)
-            pk_column = table.primary_keys()[0]
             index = find_index_by_value_at_key(
                 current_content,
-                pk_column,
-                getattr(self.klass, pk_column)
+                self._get_identifier_column_name(),
+                self._get_identifier()
             )
             current_content[index] = self.klass
-            json.dump(
-                current_content,
-                fout,
-                indent=4,
-                cls=SqlaDeclarativeEncoder
-            )
+            self._save(current_content, fout)
 
-    def delete(self, **kwargs) -> None:
-        table = SQLAIntrospector(self.klass)
-        filename = self.db.schema[table.tablename]["file"]
-
-        with open((self.db.path / filename), "w") as fout:
+    def delete(self) -> None:
+        with open(self.filepath, "w") as fout:
             # read the file -> delete some content -> overwrite the file
             current_content = json.load(fout)
-            pk_column = table.primary_keys()[0]
             index = find_index_by_value_at_key(
                 current_content,
-                pk_column,
-                getattr(self.klass, pk_column)
+                self._get_identifier_column_name(),
+                self._get_identifier()
             )
             current_content.pop(index)
-            json.dump(
-                current_content,
-                fout,
-                indent=4,
-                cls=SqlaDeclarativeEncoder
-            )
+            self._save(current_content, fout)
 
-    def read(self, *args, **kwargs) -> Union[list, DeclarativeMeta]:
-        table_inspector = SQLAIntrospector(self.klass)
-        tablename = table_inspector.tablename
-
-        filename = self.db.schema[tablename]["file"]
-
-        # if the user only passes a model instance,
-        # we need to reverse its baseclass to build a new instance from
-        cls = Registry(Base)[table_inspector.classname]
+    def _construct_instance(self, data):
+        cls = Registry(Base)[self.inspect.classname]
         cls = cls.__class__
+        return cls(**data)
 
-        with open((storage.path / filename), "r") as fout:
-            current_content = json.load(fout)
-            if not current_content:
-                return []
-            if not kwargs and not args:
-                return [cls(**data) for data in current_content]
-            if kwargs:
-                requested_column, requested_value = list(kwargs.items())[0]
-                indexes = [
-                    current_content.index(i) for i in current_content
-                    if i.get(requested_column) == requested_value
+    def _read(self, *args, **kwargs):
+        with open(self.filepath, "r") as fout:
+            current_content = self._read_file_contents()
+
+            if not args and not kwargs:
+                # This is the case for .all()
+                if not current_content:
+                    return []
+
+                return [
+                    self._construct_instance(
+                        entity
+                    ) for entity in current_content
                 ]
-                return [cls(**current_content[index]) for index in indexes]
-            elif args:
-                requested_column = SQLAIntrospector(model_class).primary_keys()[0]
-                requested_id = args[0]
+
+            if args:
+                # This is the case for .get()
+                pk = args[0]
+
+                if not current_content:
+                    raise ObjectNotFound(
+                        f"Object of type '{self.klass.__name__}' "
+                        f"with primary key '{pk}', "
+                        f"could not be found."
+                    )
+
                 index = find_index_by_value_at_key(
                     current_content,
-                    requested_column,
-                    requested_id
+                    self._get_identifier_column_name(),
+                    pk
                 )
                 item = current_content[index]
-                return cls(**item)
+                if not item:
+                    raise ObjectNotFound(
+                        f"Object of type '{self.klass.__name__}' "
+                        f"with primary key '{pk}', "
+                        f"could not be found."
+                    )
+
+                return self._construct_instance(item)
+
+            if kwargs:
+                # This is the case for .filter()
+                if not current_content:
+                    return []
+
+                results = []
+                for entity in current_content:
+                    results.append(
+                        all([
+                            entity.get(
+                                column
+                            ) == value for column, value in kwargs.items()
+                        ])
+                    )
+
+                entities = []
+                for index, matches_filter_params in enumerate(results):
+                    if matches_filter_params:
+                        entities.append(
+                            self._construct_instance(current_content[index])
+                        )
+
+                return entities

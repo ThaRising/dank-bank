eb1bf0681e1d05b28503bd18d5ffd2f1de94e463
ThaRising <kochbe.ber@gmail.com>
2020-12-25 20:11:56 +0100

    Brought main.py to execute as-is state

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import datetime
-
+from src.ui import TUI
 from src.storage import Storage
-from src.storage.models import Kunde, Konto
 
 
 if __name__ == '__main__':
-    Storage("sql")
-    Storage().db.create()
-
-    kunde = Kunde(
-        name="Ben Koch",
-        username="ben.koch",
-        password="security420",
-        plz="16386",
-        stadt="Berlin",
-        strasse="fgr rgtert ergert ergtert",
-        geb_date=datetime.date(2000, 1, 1)
-    )
-    kunde.objects.save()
-    print(kunde.objects.all())
-    print(Kunde.objects.all())
+    storage = Storage("sql")
+    tui = TUI(storage)
+    tui.mainloop()

77b255a4f64ea6ea4d3c297f537d15de4f2bb6c1
ThaRising <kochbe.ber@gmail.com>
2021-01-03 00:39:02 +0100

    Refactor input conversion tests for TUI

--- tests/unit/test_ui.py
+++ tests/unit/test_ui.py
@@ -1,15 +1,13 @@
 from src.ui import UI
 
+TEST_DATA = [
+    ("13,45", 1345),
+    ("13", 1300),
+    ("13,1", 1310),
+    ("13.45", 1345),
+]
+
 
 def test_ui():
-    sample_user_input = "13,45"
-    deposit = UI._format_deposit(sample_user_input)
-    print(deposit)
-
-    sample_user_input = "13"
-    deposit = UI._format_deposit(sample_user_input)
-    print(deposit)
-
-    sample_user_input = "13,1"
-    deposit = UI._format_deposit(sample_user_input)
-    print(deposit)
+    for user_input, formatted_value in TEST_DATA:
+        assert UI._format_deposit(user_input) == formatted_value
